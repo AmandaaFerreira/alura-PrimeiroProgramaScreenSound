75f945913448ebac0e152e6a6b6bee5645796d2b
Amanda Picinini <amandaferreirapicinini@gmail.com>
2023-11-16 23:33:27 -0300

    inclusao de novas classes, banda e album

---
+++ PrimeiroProgramaC#/ScreenSound/Program.cs
@@ -0,0 +1,168 @@
+﻿// Screen Sound 
+using System.Collections;
+
+string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+//List<string> ListaDasBandas = new List<string>();  
+
+Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
+
+
+
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+    Console.WriteLine(mensagemDeBoasVindas);
+   
+}
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("Digite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda ");
+    Console.WriteLine("Digite 4 para exibir uma média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção:");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+    
+    switch (opcaoEscolhidaNumerica)
+    {
+             case 1: RegistrarBanda();
+            break;
+            case 2: MostrarBandasRegistradas();
+            break;
+            case 3: AvaliarUmaBanda();
+            break;
+            case 4: MediaDaBanda();
+            break;
+            case -1: Console.WriteLine("Tchau Tchau" + opcaoEscolhidaNumerica);
+            break;
+            default: Console.WriteLine("Opção inválida");
+            break;
+    }
+   }
+
+void RegistrarBanda()
+{
+    Console.Clear();
+    ExibirTitulosDaOpcao("Registro das bandas");
+    Console.Write("Digite o nome da banda que deseja registrar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    bandasRegistradas.Add(nomeDaBanda, new List<int>());
+    Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+
+
+
+}
+
+void MostrarBandasRegistradas()
+{
+    Console.Clear();
+    ExibirTitulosDaOpcao("Exibindo todas as bandas registradas");
+    //for (int i = 0; i < ListaDasBandas.Count; i++)
+   // {
+       // Console.WriteLine($"Banda: {ListaDasBandas[i]}");
+   // }
+
+    foreach(string banda in bandasRegistradas.Keys)
+    {
+        Console.WriteLine($"Banda: {banda} ");
+    }
+
+    Console.WriteLine("\n Digite uma tecla para voltar ao menu principal");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
+
+void ExibirTitulosDaOpcao(string titulo)
+{
+    int quantidadeDeLetras = titulo.Length;
+    string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
+    Console.WriteLine(asteriscos);
+    Console.WriteLine(titulo);
+    Console.WriteLine(asteriscos + "\n");
+}
+
+void AvaliarUmaBanda()
+{
+    //digite qual bando voce deseja avaliar 
+    //se a banda existir no dicionário (atribuir nota)
+    //senão, voltar ao menu principal
+    Console.Clear();
+    ExibirTitulosDaOpcao("Avaliar banda");
+    Console.Write("Digite o nome da banda que voce deseja avaliar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    if (bandasRegistradas.ContainsKey(nomeDaBanda))
+    {
+        Console.Write($"Qual a nota que a banda {nomeDaBanda} merece: ");
+        int nota = int.Parse(Console.ReadLine()!);
+        bandasRegistradas[nomeDaBanda].Add(nota);
+        Console.WriteLine($" \n A nota {nota} foi registrada com sucesso para a banda {nomeDaBanda}");
+        Thread.Sleep(4000);
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }else
+    {
+    Console.WriteLine($" \n A banda {nomeDaBanda} não foi encontrada!");
+        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+
+    }
+   
+
+}
+//digite qual bando voce deseja exibir media 
+//se a banda existir no dicionário (atribuir media)
+//senão, voltar ao menu principal
+void MediaDaBanda()
+{
+
+    Console.Clear();
+    ExibirTitulosDaOpcao("Exibir Média da banda");
+    Console.Write("Digite o nome da banda que deseja saber a média: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    if (bandasRegistradas.ContainsKey (nomeDaBanda))
+    {
+        List<int> notasDaBanda = bandasRegistradas[nomeDaBanda];
+        Console.WriteLine($" \n A média da banda {nomeDaBanda} é {notasDaBanda.Average()}");
+        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
+    else
+    {
+        Console.WriteLine($" \n A banda {nomeDaBanda} não foi encontrada!");
+        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
+
+
+}
+
+
+
+ExibirLogo();
+ExibirOpcoesDoMenu();
+
+
+
+
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# alura-PrimeiroProgramaScreenSound
-Primeiro Programa em C# - Desenvolvendo uma aplicação com conceitos basicos de C# - Projeto streaming de músicas 
